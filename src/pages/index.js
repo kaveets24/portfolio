d15130a220078526a57e342b23c21f3eb4fe9d0e
Steven Kveton <steven@Stevens-MacBook-Pro.local>
2019-09-07 11:39:57 -0500

    fixed webpack named import error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/Layout/index.js";
+import Layout from "../components/layout";
 import SEO from "../components/seo";
-import Form from "../components/Contact";
+import Contact from "../components/Contact";
 
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <a href="./static/steven.kveton.pdf" target="__blank">Resume</a>
-    <Form/>
+    <Contact/>
   </Layout>
 )
 
